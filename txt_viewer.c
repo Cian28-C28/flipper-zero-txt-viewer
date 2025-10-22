b60425e72533ffcfdd421ad85c4447232d3c1132
C28 <61575135+Cian28-C28@users.noreply.github.com>
2025-10-22 01:59:43 -0400

    Fix function call for dialog mesFix txt_viewer.c: use dialog_message_show and remove unused guisage display

--- txt_viewer.c
+++ txt_viewer.c
@@ -7,7 +7,6 @@ int32_t txt_viewer_app(void* p) {
     UNUSED(p);
 
     // Open required records
-    Gui* gui = furi_record_open("gui");
     DialogsApp* dialogs = furi_record_open("dialogs");
     Storage* storage = furi_record_open("storage");
 
@@ -15,12 +14,11 @@ int32_t txt_viewer_app(void* p) {
     storage_common_mkdir(storage, "/ext/txt_viewer");
 
     // Show informational message to user
-    dialogs_message_show(dialogs, "TXT Viewer", "Folder /ext/txt_viewer created.\nAdd text files there using QFlipper.", NULL, false);
+    dialog_message_show(dialogs, "TXT Viewer", "Folder /ext/txt_viewer created.\nAdd text files there using QFlipper.", NULL, false);
 
     // Close records
     furi_record_close("storage");
     furi_record_close("dialogs");
-    furi_record_close("gui");
 
     return 0;
 }
